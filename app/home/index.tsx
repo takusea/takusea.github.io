db9aceea524c775b66246319b0e431ee461320ad
Takusea <takusea826@gmail.com>
2026-01-06 01:03:08 +0900

    feat: リンクを更新

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,31 +1,29 @@
 import {
+	IconBrandBluesky,
 	IconBrandGithub,
 	IconBrandX,
 	IconBrandYoutube,
 	IconDeviceTv,
+	IconNews,
+	type Icon,
 } from "@tabler/icons-react";
 import footerImage from "./footer.png";
 
 export function Home() {
 	return (
-		<main className="flex flex-col gap-8 p-4 max-w-screen-sm mx-auto min-h-screen">
+		<main className="flex flex-col gap-4 p-4 max-w-screen-sm mx-auto min-h-screen">
 			<h1 className="text-4xl font-bold">たくしいのうぇぶさいと</h1>
-			<p>たくしいのうぇぶさいとです。かっこかり。</p>
-			<div className="grid grid-cols-[repeat(auto-fill,minmax(min(200px,100%),1fr))] items-center gap-4">
-				{socials.map((social) => (
-					<a
-						key={social.href}
-						href={social.href}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="flex items-center gap-2 p-4 text-white font-semibold border border-white/10 rounded-lg"
-						style={{ backgroundColor: social.color }}
-					>
-						<social.icon />
-						<span>{social.name}</span>
-					</a>
-				))}
-			</div>
+			<p>たくしいのうぇぶさいとです。</p>
+			<p>お絵描き・動画投稿・プログラミング・ゲームなどをするひとです。</p>
+			<h2 className="text-xl font-bold">SNS</h2>
+			<p>XとBlueskyで大体同じポストを上げてます。（クロスポスト）</p>
+			<LinkGridView links={sns} />
+			<h2 className="text-xl font-bold">Video</h2>
+			<p>YouTubeとニコニコで大体同じ動画を上げてます。</p>
+			<LinkGridView links={video} />
+			<h2 className="text-xl font-bold">Tech</h2>
+			<p>開発とか</p>
+			<LinkGridView links={tech} />
 			<footer className="flex justify-center mt-auto text-white">
 				<small>©2025 takusea</small>
 			</footer>
@@ -39,7 +37,32 @@ export function Home() {
 	);
 }
 
-const socials = [
+type Link = {
+	href: string;
+	name: string;
+	icon: Icon;
+	color: string;
+};
+
+const LinkGridView = (props: { links: Link[] }) => (
+	<div className="grid grid-cols-[repeat(auto-fill,minmax(min(200px,100%),1fr))] items-center gap-4">
+		{props.links.map((link) => (
+			<a
+				key={link.href}
+				href={link.href}
+				target="_blank"
+				rel="noopener noreferrer"
+				className="flex items-center gap-2 p-4 text-white font-semibold border border-white/10 rounded-lg"
+				style={{ backgroundColor: link.color }}
+			>
+				<link.icon />
+				<span>{link.name}</span>
+			</a>
+		))}
+	</div>
+);
+
+const sns = [
 	{
 		href: "https://x.com/takusea",
 		name: "X (旧Twitter)",
@@ -47,11 +70,14 @@ const socials = [
 		color: "#000000",
 	},
 	{
-		href: "https://github.com/takusea",
-		name: "GitHub",
-		icon: IconBrandGithub,
-		color: "#171515",
+		href: "https://bsky.app/profile/takusea.com",
+		name: "Bluesky",
+		icon: IconBrandBluesky,
+		color: "#006aff",
 	},
+];
+
+const video = [
 	{
 		href: "https://youtube.com/c/takusea",
 		name: "YouTube",
@@ -63,5 +89,20 @@ const socials = [
 		name: "ニコニコ",
 		icon: IconDeviceTv,
 		color: "#252525",
+	},
+];
+
+const tech = [
+	{
+		href: "https://blog.takusea.com",
+		name: "Blog",
+		icon: IconNews,
+		color: "#00ab86",
+	},
+	{
+		href: "https://github.com/takusea",
+		name: "GitHub",
+		icon: IconBrandGithub,
+		color: "#171515",
 	},
 ];
