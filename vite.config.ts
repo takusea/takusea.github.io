d96df7534f41ba73ab40f6c4a5b8322e837ab3e0
Takusea <takusea826@gmail.com>
2026-06-16 00:09:44 +0900

    chore: ViteのtsconfigPathsオプションをオンに変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	resolve: { tsconfigPaths: true },
 	plugins: [reactRouter(), tailwindcss()],
 });
