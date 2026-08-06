2dcc2998f3183bb9f6579a3dc6c8863ec2f2f723
Takusea <takusea826@gmail.com>
2026-08-06 23:06:06 +0900

    feat: デザインを調整

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Card = (props: Props) => (
 	<div
-		className="flex flex-col gap-2 p-4 bg-white/50 dark:bg-black/50 backdrop-blur-sm border-solid border-12 [border-image:url(/border-2.png)_6]"
+		className="flex flex-col gap-2 p-4 rounded-xl bg-white/50 dark:bg-black/50 backdrop-blur-sm border-solid border-12 [border-image:url(/border-2.png)_6]"
 		style={{ imageRendering: "pixelated" }}
 	>
 		{props.children}
