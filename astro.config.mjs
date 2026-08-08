deef5c235ca81b0f684f6762cf2a06f6434bb7fe
Takusea <takusea826@gmail.com>
2026-08-08 18:02:10 +0900

    chore: lintを実行

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  integrations: [react()],
-  vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "~": fileURLToPath(new URL("./app", import.meta.url)),
-      },
-    },
-  },
+	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"~": fileURLToPath(new URL("./app", import.meta.url)),
+			},
+		},
+	},
 });
